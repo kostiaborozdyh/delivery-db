5a50cb3b01d7e07aebe5ba24dd59e27d79ef76e4
Kostia Borozdyh <kostiaborozdyh@gmail.com>
2022-12-20 10:38:57 +0200

    add pageable

--- src/main/java/com/delivery/db/repository/ReviewRepository.java
+++ src/main/java/com/delivery/db/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.delivery.db.repository;
 
 import com.delivery.db.entities.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    Page<Review> findAll(Pageable pageable);
 }
--- src/main/java/com/delivery/db/repository/UserRepository.java
+++ src/main/java/com/delivery/db/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.delivery.db.repository;
 
 import com.delivery.db.entities.Role;
 import com.delivery.db.entities.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByLogin(String login);
-    List<User> findByRoleIsNot(Role role);
+    List<User> findByRoleIsNot(Role role, Pageable pageable);
     Optional<User> findById(Integer id);
     @Modifying
     void deleteUserById(Integer id);
